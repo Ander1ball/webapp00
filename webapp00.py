1fa16135d48c01c3cbc0e7856fb45f1a67e2fd81
Ander1ball <116382092+Ander1ball@users.noreply.github.com>
2022-10-22 00:18:29 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,25 +1,26 @@
 # myFirstStreamlitApp.py
-
-#import the library
+#import the libraries
 import streamlit as st
+from PIL import Image
 
-image01 = Image.open('steve-e-alex-minecraft.webp')
-
+image01 = Image.open('desenvolvimento.jpg')
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
 st.title("Minecreft")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("por que o jogo é bom pra pensar?")
+st.header("Historia do jogo quadrado")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("O jogo conhecido como o jogo quadrado teve o seu lançamento em 18 de novembro de 2011, onde foi notado muitas caracteristicas marcantes do jogo,como por exemplo, o fato dele ser totalmente quadrado em até mesmo em seus personagens.Seu personagens no inicio eram somente um o canonico Steve, mas em setembro de 2014 foi adicionada a personagem Alex.")
+st.subheader(")
 
 # Use st.write("") para adicionar um texto ao seu Web app
-st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+st.write("O jogo conhecido como o jogo quadrado teve o seu lançamento em 18 de novembro de 2011, onde foi notado muitas caracteristicas marcantes do jogo,como por exemplo, o fato dele ser totalmente quadrado em até mesmo em seus personagens.Seu personagens no inicio eram somente um o canonico Steve, mas em setembro de 2014 foi adicionada a personagem Alex.")
 
-menu = esc ["Texto_Colunas",
-            "Texto_Markdown",
-            "Inserir_Figura"]
+st.subheader("------ **Desenvolvido por: Matheus Terencio(pq ele é foda)** -----")
+
+menu = ["Texto_Colunas",
+        "Texto_Markdown",
+        "Inserir_Figura"]
 choice = st.sidebar.selectbox("Menu de Opções",menu)
 st.sidebar.write("Texto Side Bar")
 
@@ -48,5 +49,3 @@ elif choice == "Texto_Markdown":
     )
 elif choice == "Inserir_Figura":
     st.image(image01, width=800, caption='Rótulo da Imagem 01') 
-    
-    
