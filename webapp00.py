74879ca7d7d90468351fed6f83409051d6e1f9ad
Ander1ball <116382092+Ander1ball@users.noreply.github.com>
2022-10-22 00:08:13 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -2,8 +2,6 @@
   
 #import the library
 import streamlit as st
-  
-st.image(image01, width=800, caption='steve-e-alex-minecraft.webp') 
 
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
 st.title("Minecreft")
@@ -17,35 +15,7 @@ st.subheader("O jogo conhecido como o jogo quadrado teve o seu lançamento em 18
 # Use st.write("") para adicionar um texto ao seu Web app
 st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
 
-menu = ["Texto_Colunas",
-        "Texto_Markdown",
-        "Inserir_Figura"]
-choice = st.sidebar.selectbox("Menu de Opções",menu)
-st.sidebar.write("Texto Side Bar")
-    
-if choice == "Texto_Colunas":       
-    st.subheader("Texto formatado em colunas")
-    st.write("Veja a seguir uma opção de formatação em colunas")    
-    cols01 = st.columns(2)    
-    cols01[0].write('Texto da Coluna 01')
-    cols01[1].write('Texto da Coluna 02')
-elif choice == "Texto_Markdown":
-    st.subheader("Texto Markdown")
-    st.write("Veja a seguir opção de formatação de texto Markdown")
-    st.markdown(
-    """
-    ## *Alguns sites referências*:    
-    - [Streamlit: hello world](https://calmcode.io/streamlit/hello-world.html)
-    - [:star: Streamlit emoji shortcodes](https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlitapp.com/)
-    - [Layouts and Containers](https://docs.streamlit.io/library/api-reference/layout)
-   
-    ##### CRONOGRAMA
-    DIA | CH HORÁRIA | CONTEÚDO
-    :---------: | :------: | :-------:
-    Dia 1 de 2 | ?h | ? a ?
-    Dia 2 de 2 | ?h | ? a ?
-    """
-    )
+       ["Inserir_Figura"]
 elif choice == "Inserir_Figura":
     st.image(image01, width=800, caption='Rótulo da Imagem 01') 
     
